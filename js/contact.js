b0866d1be4e78ecc2cd9b4d1ade2e272262ffa93
gichul <gichul0317@gmail.com>
2021-05-31 22:09:52 -0400

    js functionality for humburger button and form added on contact.html

---
+++ js/contact.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const form = document.querySelector('form');
+const navBtn = document.querySelector('.nav-btn');
+const nav = document.querySelector('.active');
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const userName = document.querySelector('#user-name');
+  const userMail = document.querySelector('#user-email');
+  const userComment = document.querySelector('#message');
+  if (userName.value && userMail.value && userComment.value) {
+    alert('Thank You. We will get back to you soon 😉😂');
+  }
+});
+
+navBtn.addEventListener('click', function () {
+  nav.classList.toggle('active');
+});
